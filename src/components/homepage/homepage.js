b8a2dc928e74c629e68510cdbf993d7ecbb10b14
Marc Molta <mcmolta10@gmail.com>
2018-11-15 18:03:43 -0500

    remove comments

--- src/components/homepage/homepage.js
+++ src/components/homepage/homepage.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 
 import './homepage.css';
 
-// homepage background gif 
-// https://thumbs.gfycat.com/PastEssentialCero-small.gif
-
 class Homepage extends Component {
     // @TODO: replace this, obviously
     clickJawn = e => {
@@ -18,11 +15,7 @@ class Homepage extends Component {
             <h1 id="homepage-header">Temporary homepage for crash data tool</h1>
 
             <hr />
-            {/*
-                Future goals:
-                Container that has a bunch of 'tiles' that each link to different apps
-                the web viewer will be one of several. For now it's just a div
-            */}
+
             <section id="crash-apps-container">
                 <div class="crash-app">
                     <img src="https://cdn0.iconfinder.com/data/icons/car-crash/500/hit-2-512.png" alt="hot rod" class="crash-app-img" />
