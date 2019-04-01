98e96a8e672b342539c3621e807ce051d6a1dd33
Marc Molta <mcmolta10@gmail.com>
2019-04-01 11:08:28 -0400

    update passSearchToAPI

--- react/src/components/search/handleForm.js
+++ react/src/components/search/handleForm.js
@@ -1,14 +1,30 @@
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
 import { counties, munis, states } from './dropdowns.js'
+
 
 ////
 // Functions to handle parsed form inputs
 ////
-const passSearchToAPI = async (type, boundary) => {
-    const api = `https://a.michaelruane.com/api/crash-data/v1/sidebarInfo?type=${type}&value=${boundary}`
+const options = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+    }
+}
 
-    // @TODO: add options to fetch
-    const stream = await fetch(api)
+const passSearchToAPI = async boundaryObj => {
+
+    // destructure boundaryObj to get the type and boundary values
+    const {type, boundary} = boundaryObj
+    const api = `https://a.michaelruane.com/api/crash-data/v1/sidebarInfo?type=${type}&value=${boundary}`
+    const stream = await fetch(api, options)
+
+    // error handling - pass the failure message + the boundary object to give context to the displayed error response
+    if(!stream.ok) return { fail: stream.statusText, boundaryObj }
+
     const response = await stream.json()
+    
     console.log('response is ', response)
 
     // @TODO: put response in the Context tree so sidebar.js can consume it
@@ -16,20 +32,23 @@ const passSearchToAPI = async (type, boundary) => {
 
 // runs the mapboxgl geocoder to turn address or boundary into coordinates
 const geocode = jawn => {
+    const accessToken = 'pk.eyJ1IjoibW1vbHRhIiwiYSI6ImNqZDBkMDZhYjJ6YzczNHJ4cno5eTcydnMifQ.RJNJ7s7hBfrJITOBZBdcOA'
+    const geocoder = new MapboxGeocoder({accessToken})
     console.log('geocoding ', jawn)
     return jawn
 }
 
-// passes a geocoded jawn and (optionally) a boundary to map.js
-const updateMap = (coords, boundary) => {
-    console.log('hit update map')
+// passes a geocoded jawn and (optionally) a boundary object to map.js
+const updateMap = (coords, boundaryObj) => {
+    console.log('hit update map ')
+
     // geocode coordinates if necessary
-    if(!coords) coords = geocode(boundary)
+    if(!coords) coords = geocode(boundaryObj.boundary)
 
     // put this jawn in the Context tree so that map.js can read from it
     const output = {
         coords,
-        boundary
+        boundaryObj
     }
 }
 
@@ -80,8 +99,10 @@ const submitSearch = e => {
 
     // get data from db and then geocode the boundary so that the map can update and do all the necessary filtering
     if(apiCall) {
-        passSearchToAPI(type, boundary)
-        updateMap(false, boundary)
+        const boundaryObj = { type, boundary }
+        
+        passSearchToAPI(boundaryObj)
+        updateMap(false, boundaryObj)
 
     // update the map let it handle hitting the API (map.flyTo() ==> map.LngLatBounds() ==> fetch(`endpoint-that-handles-geometry-envelopes`))
     }else{
