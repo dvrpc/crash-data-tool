4fb53fd98442b61628367484f26532972d7b04a4
Marc Molta <mcmolta10@gmail.com>
2019-10-31 11:56:28 -0400

    redesign modal with tabs

--- react/src/components/navbar/navbar.js
+++ react/src/components/navbar/navbar.js
@@ -18,21 +18,38 @@ class Navbar extends Component {
         <nav id="navbar">
           <div>
             <Link id="crash-map-nav-home" to="/">HOME</Link>
-            <dialog id="crash-map-nav-modal" ref={el => this.modal = el}>
-              <button type="button" className="crash-map-nav-close-modal" id="crash-map-nav-x" onClick={this.hideModal}>X</button>
-              <h1>How to Use This App</h1>
-              <hr />
-              <ul>
-                <li>How to filter with the navbar</li>
-                <li>
-                  How to interact with the map
-                  <ul>
-                    <li>Clicking on municipalities</li>
-                    <li>How to draw polygons</li>
-                  </ul>
-                </li>
-                <li>How to interact with the sidebar</li>
-              </ul>
+            <dialog id="crash-map-modal" ref={el => this.modal = el}>
+              <button type="button" className="crash-map-close-modal" id="crash-map-modal-x" onClick={this.hideModal}>X</button>
+              <h2>DVRPC Crash Data Viewer</h2>
+              <div id="crash-map-modal-tabs">
+                <h3 id="crash-map-modal-active-tab">About</h3>
+                <h3>How To</h3>
+                <h3>Contact</h3>
+                <h3>Disclaimer</h3>
+              </div>
+              <div id="crash-map-modal-content">
+                <ul>
+                  <li>How to filter with the navbar</li>
+                  <li>
+                    How to interact with the map
+                    <ul>
+                      <li>Clicking on municipalities</li>
+                      <li>How to draw polygons</li>
+                    </ul>
+                  </li>
+                  <li>How to interact with the sidebar</li>
+                  <li>
+                      Alternatively, we could have tabs
+                      <ul>
+                        <li>About</li>
+                        <li>How To</li>
+                        <li>Contact</li>
+                        <li>Disclaimer</li>
+                      </ul>
+
+                  </li>
+                </ul>
+              </div>
             </dialog>
             <button id="crash-map-nav-info-btn" type="button" onClick={this.showModal}>i</button>
           </div>
