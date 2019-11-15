77be53471bb7fca6a0233cb50e1bed663e9c92e2
Marc Molta <mcmolta10@gmail.com>
2019-11-15 16:42:38 -0500

    refactor popup function and set up pagination html + functions #12

--- react/src/components/map/map.js
+++ react/src/components/map/map.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import ReactPaginate from 'react-paginate';
 import { connect } from 'react-redux'
 import mapboxgl from "mapbox-gl";
 import MapboxDraw from '@mapbox/mapbox-gl-draw';
@@ -32,6 +31,11 @@ class Map extends Component {
             })
         }
     }
+    
+
+    /**********************/
+    // Lifecycle Methods //
+    /**********************/
 
     componentDidMount() {
         mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
@@ -120,6 +124,8 @@ class Map extends Component {
 
                 // extract array of crn and severity for all crashes at that clicked point
                 const crnArray = features.map(crash => { return {crn: crash.properties.id, severity: crash.properties.max_sever} })
+                const length = crnArray.length
+                let index = 0
 
                 // initialize the mapbox popup object
                 const popup = new mapboxgl.Popup({
@@ -127,40 +133,40 @@ class Map extends Component {
                     closeOnClick: true
                 }).setLngLat(e.lngLat)
 
-                // get info
-                const popupInfo = popups.getPopupInfo(crnArray[0])
+                // get info and set popup contents
+                this.handlePopup(crnArray, index, length).then(html => popup.setHTML(html).addTo(this.map))
+                
+                // handle pagination if necessary
+                if (length > 1) {
+                    // get a handle on the buttons
+                    const node = ReactDOM.findDOMNode(this)
+                    let nextPopup, previousPopup
 
-                popupInfo.then(result => {
-                    let html;
+                    const hack = document.getElementById('crash-next-popup')
+                    console.log('hack will work tho... ', hack)
 
-                    // create popup content (success or fail)
-                    if(result.fail){
-                        html = popups.catchPopupFail(result.crn)
-                    }else{
-                        html = popups.setPopup(result)
+                    if (node instanceof HTMLElement) {
+                        console.log('node bruh ', node)
+
+                        /// the buttons cant be accessed this way. It's on the node but doesn't querySelect probably b/c of the async nature of this.handlePopup
                     }
 
-                    // add the popup to the map
-                    popup.setHTML(html).addTo(this.map)
-                })
+                    console.log('next popup btn ', nextPopup)
+                    console.log('previous popup btn ', previousPopup)
 
-                // add pagination if necessary
-                if (crnArray.length > 1) {
-                                        
-                    const paginate = <ReactPaginate
-                    previousLabel={'previous'}
-                    nextLabel={'next'}
-                    breakLabel={'...'}
-                    pageCount={crnArray.length}
-                    pageRangeDisplayed={5}
-                    />
-                                        
-                    // big issue with this approach
-                        // ReactDOM.createPortal needs to be called in the return of a components render method
-                        // a potential work around is to create a wrapper component that accepts the popup as props, creates the paginate element and then calls createPortal in the render method, placing <Paginate /> within popup. 
-                            // since paginate would exist within another component, the click handlers could also go there. 
-                    
-                    //ReactDOM.createPortal(paginate, popup)
+                    // add next click handler
+                    // nextPopup.onclick = () => {
+                    //     index -= 1 < 0 ? index = 0 : index -= 1
+                    //     //  removed "addTo(this.map)" b/c the popup is already on, just want to overwrite the content
+                    //     this.handlePopup(crnArray, index, length).then(html => popup.setHTML(html))
+                    // }
+
+                    // // add previous click handler
+                    // previousPopup.onclick = () => {
+                    //     index += 1 >= length ? index = length - 1 : index += 1
+                    //     //  removed "addTo(this.map)" b/c the popup is already on, just want to overwrite the content
+                    //     this.handlePopup(crnArray, index, length).then(html => popup.setHTML(html))
+                    // }
                 }
             })
         })
@@ -471,6 +477,24 @@ class Map extends Component {
 
         this.props.getPolygonCrashes(bboxFormatted)
         this.props.getData(boundaryObj)
+    }
+
+    // handle popup creation and pagination
+    handlePopup = async (crns, index, length) => {
+        const popupInfo = popups.getPopupInfo(crns[index])
+
+        const html = await popupInfo.then(result => {
+            const current = index + 1
+
+            // create popup content (success or fail)
+            if(result.fail){
+                return popups.catchPopupFail(result.crn)
+            }else{
+                return popups.setPopup(result, current, length)
+            }
+        })
+
+        return html
     }
 
 
