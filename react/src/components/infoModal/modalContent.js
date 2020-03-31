c530412403f648b043fefdb70c81b313a3ca34bc
mmolta <mcmolta10@gmail.com>
2020-03-31 17:51:42 -0400

    clean up modal content

--- react/src/components/infoModal/modalContent.js
+++ react/src/components/infoModal/modalContent.js
@@ -23,13 +23,12 @@ export const modalContent = {
       <ul>
         <li>
           <strong>Interact with the Sidebar</strong><br />
-          <span>View crash statistics for the selected geography (the default geography is the DVRPC Region). Interact with the data by toggling the forms to change the date range and crash types.</span><br />
-          <span>Crash data is visualized in a series of charts that give a detailed breakdown of trend, severity, mode and crash type.</span><br />
+          <span>View crash statistics for the selected geography in a series of charts that give a detailed breakdown of trend, severity, mode and crash type.</span><br />
+          <span>Interact with the data by toggling the forms to change the date range and crash types.</span><br />
         </li>
         <li>
           <strong>Interact with the Map</strong><br />
-          <span>At the county zoom level, hover over counties to see their name and click on them to get crash statistics for that county.</span></br >
-          <span>At the municipality zoom level, hover over municipalities to see their name and click on them to get crash statistics for that municipality.</span><br />
+          <span>Hover counties or municipalities to see their name and click on them to get crash statistics for that county or municipality.</span></br >
           <span>Click on the polygon map overlay to draw a polygon over a selected area and get crash statistics for the selected area.</span><br />
           <span>Click on the DVRPC logo map overlay to recenter the map on the entire region.</span>
         </li>
