9eb234657ad19f20c30480a71198d5d2a6c18f47
Kris Warner <kdwarn@protonmail.com>
2020-05-19 14:17:34 -0400

    update tests for incorrect source data

--- api/tests/test_get_summary.py
+++ api/tests/test_get_summary.py
@@ -15,12 +15,11 @@ def test_unknown_geoid_return_404(client):
 
 
 @pytest.mark.parametrize(
-    "area,value", [("state", "CA"), ("county", "Allegheny"), ("municipality", "Erie City"),],
+    "area,value", [("state", "CA"), ("county", "Allegheny"), ("municipality", "Erie City")],
 )
 def test_unknown_values_return_404(client, area, value):
     response = client.get(endpoint + f"?{area}={value}")
     data = response.json()
-    print(data)
     assert response.status_code == 404
     assert data["message"] == "No information found for given parameters"
 
@@ -221,9 +220,9 @@ def test_summed_collision_types_equals_total_crashes(client, area, value, ksi_on
     assert yr_18_sum_collisions == data["2018"]["total crashes"]
 
 
-###################
-# COMPARING TO DB #
-###################
+############################
+# COMPARING TO SOURCE DATA #
+############################
 
 
 def test_data_Chadds_Ford(client):
@@ -442,3 +441,57 @@ def test_data_PA(client):
     assert y_18["mode"]["ped"] == 2272
     assert y_18["mode"]["vehicle occupants"] == 88096 - 489 - 2272
     assert total_injured_18 == 26125  # db has 26129, but sum of components is 29125
+
+
+@pytest.mark.parametrize(
+    "county,crashes14,crashes15,crashes16",
+    [
+        ("Burlington", 13391, 12566, 12258),
+        ("Camden", 14723, 12772, 12672),
+        ("Gloucester", 6643, 7172, 7294),
+        ("Mercer", 12907, 12406, 12214),
+    ],
+)
+def test_total_crashes_by_NJ_county(client, county, crashes14, crashes15, crashes16):
+    response = client.get(endpoint + f"?county={county}")
+    data = response.json()
+
+    assert data["2014"]["total crashes"] == crashes14
+    assert data["2015"]["total crashes"] == crashes15
+    assert data["2016"]["total crashes"] == crashes16
+
+
+@pytest.mark.parametrize(
+    "county,injured14,injured15,injured16",
+    [
+        ("Burlington", 4099, 3936, 4055),
+        ("Camden", 4930, 4819, 4735),  # db- 4816 (2015) and 4734 (2016); nums here sums
+        ("Gloucester", 2257, 2527, 2546),
+        ("Mercer", 3351, 3486, 3629),  # db has 3350 (2014), 3482 (2015); nums here sums
+    ],
+)
+def test_total_injured_by_NJ_county(client, county, injured14, injured15, injured16):
+    response = client.get(endpoint + f"?county={county}")
+    data = response.json()
+
+    total_injured_14 = (
+        data["2014"]["severity"]["major"]
+        + data["2014"]["severity"]["moderate"]
+        + data["2014"]["severity"]["minor"]
+        + data["2014"]["severity"]["unknown severity"]
+    )
+    total_injured_15 = (
+        data["2015"]["severity"]["major"]
+        + data["2015"]["severity"]["moderate"]
+        + data["2015"]["severity"]["minor"]
+        + data["2015"]["severity"]["unknown severity"]
+    )
+    total_injured_16 = (
+        data["2016"]["severity"]["major"]
+        + data["2016"]["severity"]["moderate"]
+        + data["2016"]["severity"]["minor"]
+        + data["2016"]["severity"]["unknown severity"]
+    )
+    assert total_injured_14 == injured14
+    assert total_injured_15 == injured15
+    assert total_injured_16 == injured16
