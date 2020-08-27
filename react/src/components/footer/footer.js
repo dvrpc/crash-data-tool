9f887c94e4d01c2f2a3f0fc37bb2efb3080bc83a
Marc Molta <mcmolta10@gmail.com>
2020-08-27 11:43:57 -0400

    update footer alignment

--- react/src/components/footer/footer.js
+++ react/src/components/footer/footer.js
@@ -27,27 +27,27 @@ class Footer extends Component {
         <div id="footer-contact-wrapper">
             <h3>Connect With Us!</h3>
             <div id="footer-social-wrapper">
-                <a id="mailto" class="footer-social-link" href="https://signup.e2ma.net/signup/1808352/1403728/" rel="noopener" target="_blank">
+                <a id="mailto" class="footer-social-link" href="https://signup.e2ma.net/signup/1808352/1403728/" rel="noopener noreferrer" target="_blank">
                     <img loading="lazy"id="email-icon" class="footer-social" src={email} alt="icon link to email" />
                 </a>
                 <span class="vr"></span>
-                <a class="footer-social-link" href="https://www.facebook.com/DVRPC" rel="noopener" target="_blank">
+                <a class="footer-social-link" href="https://www.facebook.com/DVRPC" rel="noopener noreferrer" target="_blank">
                     <img loading="lazy" class="footer-social" src={facebook} alt="icon link to facebook" />
                 </a>
                 <span class="vr"></span>
-                <a class="footer-social-link" href="https://twitter.com/DVRPC" rel="noopener" target="_blank">
+                <a class="footer-social-link" href="https://twitter.com/DVRPC" rel="noopener noreferrer" target="_blank">
                     <img loading="lazy" class="footer-social" src={twitter} alt="icon link to twitter" />
                 </a>
                 <span class="vr"></span>
-                <a class="footer-social-link" href="https://www.instagram.com/dvrpc/?hl=en" rel="noopener" target="_blank">
+                <a class="footer-social-link" href="https://www.instagram.com/dvrpc/?hl=en" rel="noopener noreferrer" target="_blank">
                     <img loading="lazy" class="footer-social" src={instagram} alt="icon link to instagram" />
                 </a>
                 <span class="vr"></span>
-                <a class="footer-social-link" href="https://www.linkedin.com/company/delaware-valley-regional-planning-commission" rel="noopener" target="_blank">
+                <a class="footer-social-link" href="https://www.linkedin.com/company/delaware-valley-regional-planning-commission" rel="noopener noreferrer" target="_blank">
                     <img loading="lazy" class="footer-social" src={linkedin} alt="icon link to linkedin" />
                 </a>
                 <span class="vr"></span>
-                <a class="footer-social-link" href="https://www.youtube.com/channel/UCEU8UI5_iGkVypHP93b5jLA" rel="noopener" target="_blank">
+                <a class="footer-social-link" href="https://www.youtube.com/channel/UCEU8UI5_iGkVypHP93b5jLA" rel="noopener noreferrer" target="_blank">
                     <img loading="lazy" class="footer-social" src={youtube} alt="icon link to youtube" />
                 </a>
             </div>
