8ea964b1b3ed1648266522576c1b6385875e4a7a
mmolta <mcmolta10@gmail.com>
2021-07-30 17:23:32 -0400

    set default select & intro range

--- react/src/components/sidebar/sidebar.js
+++ react/src/components/sidebar/sidebar.js
@@ -207,7 +207,7 @@ class Sidebar extends Component {
                 
                 <span id="crash-map-print-sidebar" onClick={this.setSrc}><img id="crash-map-print-icon" src={print} alt="print stats icon" /> print statistics</span>
                 
-                <p className="sidebar-paragraphs first-paragraph">This tool's default setting is limited to five years of killed and severe injury crashes (abbreviated as "KSI") for 2014 to 2019. Five years of data is typically used by local, state, and federal partners in safety analyses.</p>
+                <p className="sidebar-paragraphs first-paragraph">This tool's default setting is limited to five years of killed and severe injury crashes (abbreviated as "KSI") for 2015 to 2019. Five years of data is typically used by local, state, and federal partners in safety analyses.</p>
                 <p className="sidebar-paragraphs">The following charts and map are showing results for <strong>{crashType}</strong> crash types from <strong>{from}</strong> to <strong>{to}</strong> in <strong>{area}</strong>. You can adjust the range and severity type using the forms below:</p>
 
                 <form className="crash-map-charts-form" id="crash-map-update-range" onSubmit={this.updateRange}>
@@ -220,7 +220,7 @@ class Sidebar extends Component {
                                     From: 
                                     <select id="crash-select-from" name="from" className="crash-map-first-input hover-btn">
                                         <option value="2014">2014</option>
-                                        <option value="2015">2015</option>
+                                        <option value="2015" selected>2015</option>
                                         <option value="2016">2016</option>
                                         <option value="2017">2017</option>
                                         <option value="2018">2018</option>
